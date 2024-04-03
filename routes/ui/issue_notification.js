a449b4a351f7941015dd11454f92c596e2d2c73c
ali99944 <ali99944>
2024-04-03 04:00:48 +0200

    fix issue notifications

--- routes/ui/issue_notification.js
+++ routes/ui/issue_notification.js
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 const router = Router();
 import IssueNotification from '../../models/IssueNotification.js';
-import jwt from 'jsonwebtoken';
-import Manager from '../../models/Manager.js';
 
 router.get('/issues/notifications', async (req, res) => {
 
     
-    const issuenotifications = await find()
+    const issuenotifications = await IssueNotification.find()
 
     res.render('issueNotifications/index', { 
         issuenotifications: issuenotifications.reverse() ,
