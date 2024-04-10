5395825c54a6ec79837ebe7728cadf5b63c140ba
ali99944 <ali99944>
2024-04-10 07:53:31 +0200

    update dashboard design

--- utils/smtp_service.js
+++ utils/smtp_service.js
@@ -1,7 +1,6 @@
 import { createTransport } from 'nodemailer';
 import { smtp_host, smtp_pass, smtp_port, smtp_user } from '../config.js';
 
-// Create a transporter object using the default SMTP transport
 const transporter = createTransport({
   host: smtp_host, // Hostinger SMTP server
   port: smtp_port, // Port for sending mail
